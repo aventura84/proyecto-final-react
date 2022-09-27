81ee5adb29eab00b704aff841698d519f548eb95
aventura84 <ventura_antonio@hotmail.com>
2022-09-27 21:09:14 +0200

    post.css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import Home from "./Home/Home";
+import Post from './Post';
 
 function App() {
   return (
@@ -16,7 +17,10 @@ function App() {
         <Route path="signup" element={<Signup />} />
       </Routes>
       <Footer />
-    </main>
+        </main>
+        <Post/>
+        {/*Posts */}
+        {/*Posts */}
   );
 }
 
