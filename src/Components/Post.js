a906f076a33137edf8195415c59b40496b296ae6
aventura84 <ventura_antonio@hotmail.com>
2022-09-28 20:55:33 +0200

    base de datos

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import'./Post.css';
-function Post(){
-    return (
-        <div className="post">
-            <h3>Username</h3>
-{/*header->avatar + username*/}
-<img className="post__image"src="https://www.freecodecamp.org/news/content/images/"
-{/*image*/}
-<h4 classNamecleverqazi="post_text": WOW day three of live sessions</h4>
-{/*username+caption*/}
-        </div>
-    )
+import React from "react";
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
+function Post({ username, caption, imageUrl }) {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post_avatar"
+          alt="RafehQazi"
+          src="static/images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+      {/*header->avatar + username*/}
+      <img className="post__image" src={imageUrl} alt="" />
+
+      <h4 className="post_text">
+        <strong>cleverqazi</strong> {caption}
+      </h4>
+      {/*username+caption*/}
+    </div>
+  );
 }
 
-export default Post
+export default Post;
